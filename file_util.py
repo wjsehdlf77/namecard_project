73aef0e7319ab194e7ab08ffc303de22f00e39ed
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-07 23:10:40 +0900

    진전이없다...

--- file_util.py
+++ file_util.py
@@ -1,17 +1,17 @@
 
 import json 
 
-def save_json(file_name, data):
+def save(file_name, data):
     try:
-        with open(file_name,'wt') as file:
+        with open(file_name,'wb') as file:
             json.dump(data, file)
 
     except Exception as e:
         print(e)
 
-def load_json(file_name):
+def load(file_name):
     try:
-        with open(file_name,'rt') as file:
+        with open(file_name,'rb') as file:
             data = json.load(file)
             return data
     
--- namecard.py
+++ namecard.py
@@ -1,23 +1,23 @@
-from file_util import  load_json
+from file_util import  load, save
+import os
+
 
 class NameCard:
-    def __init__(self, name, email, phone, addr):
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.addr = addr
-
-
-
-
-
-
-
-for a in load_json('addressbook.txt'):
-    print(a)
-
+    def __init__(self):
+        self.book = []
 
     
+        
+
+    def load(self, file_path):
+    
+        if os.path.exists(file_path):  
+            self.book = load(file_path)
+        else:                          
+            save(file_path, self.book)
+
+    def save(self, file_path):
+        save(file_path, self.book)
 
 
 
