3d105e4260b4ea2d386a4f1ae5dab9c488f11d6d
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-08 23:11:50 +0900

    일단 진짜 완료

--- print_save.py
+++ print_save.py
@@ -10,7 +10,10 @@ def main():
 
     #addressbook.txt를 순회하며 출력
     for ix in book.book:
-        print(ix)
+        list_book = ix.split(',')
+        print('-' * 85)
+        print(f'이름 : {list_book[0]}, 이메일 : {list_book[1]}, 휴대폰 : {list_book[2]}, 주소 : {list_book[3]}')
+        print('-' * 85)
 
     #addr.db에 저장
     book.save(NAME)
